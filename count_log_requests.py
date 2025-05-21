56b62898203679754489b491d83a932c7d8dd695
Daniel Michel <dmichel@griddynamics.com>
2025-05-21 11:09:24 -0600

    Move instructions to the beginning

--- count_log_requests.py
+++ count_log_requests.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python3
-
-from argparse import ArgumentParser
-from collections import Counter
-
 """
 3. Create a script that reads the access log from a file. The name of the file is 
 provided as an argument. An output of the script should provide the total number 
 of different User Agents and then provide statistics with the number of requests 
 from each of them.
 """
+
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+from collections import Counter
 
 parser = ArgumentParser(description='Read access log and count requests from different User Agents')
 parser.add_argument('filename', help='The name of the access log file')
@@ -24,6 +24,6 @@ with open(args.filename, "r", encoding="utf-8", errors="ignore") as file:
             hosts[host] += 1
 
 print(f"Total number of hosts: {len(hosts)}\n")
-print(f"HOST: REQUESTS")
+print("HOST: REQUESTS")
 for host, requests in hosts.most_common():
     print(f"{host}: {requests}")

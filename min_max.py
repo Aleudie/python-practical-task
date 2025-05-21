6bb840d663ce17868b5321cfb71bc667d3ecf946
Daniel Michel <dmichel@griddynamics.com>
2025-05-21 11:14:25 -0600

    Move instructions to the beginning

--- min_max.py
+++ min_max.py
@@ -1,25 +1,26 @@
-#!/usr/bin/env python3
+"""
+Task 1.2 Given a list of integers. Remove duplicates from the list 
+and create a tuple. Find the minimum and maximum number.
+"""
 
-"""
-Task 1.2 Given a list of integers. Remove duplicates from the list and create a tuple. Find the minimum and maximum number.
-"""
+#!/usr/bin/env python3
 
 class Solution:
     def min_max(self, lst: list[int]) -> tuple:
         if not lst:
             raise ValueError("The list is empty")
         return min(lst), max(lst)
-    
+
     def to_tuple(self, lst: list[int]) -> tuple:
         if not lst:
             raise ValueError("The list is empty")
         return tuple(set(lst))
 
-lst = [1, 7, 1, 3, 2, 2, 5, 4, 6, 8, 9, 0, 10, 0, 0, 0, 0, -2, -23, 55, 11, 12, 13, 14, 15]
+LST = [1, 7, 1, 3, 2, 2, 5, 4, 6, 8, 9, 0, 10, 0, 0, 0, 0, -2, -23, 55, 11, 12, 13, 14, 15]
 solution = Solution()
 
-min_value, max_value = solution.min_max(lst)
-tupl = solution.to_tuple(lst)
+min_value, max_value = solution.min_max(LST)
+tupl = solution.to_tuple(LST)
 
 print(f"Minimum value: {min_value}")
 print(f"Maximum value: {max_value}")
